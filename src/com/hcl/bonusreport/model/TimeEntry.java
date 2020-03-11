802505993d2fa20e183d62c5b836d78fc3b27b7e
Antonio Avilez <Antonio Avilez>
2020-03-11 09:18:57 -0600

    feature: read time entry for each user

--- src/com/hcl/bonusreport/model/TimeEntry.java
+++ src/com/hcl/bonusreport/model/TimeEntry.java
@@ -1,5 +1,12 @@
 package com.hcl.bonusreport.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class TimeEntry {
 
 	private String date;
@@ -8,5 +15,60 @@ public class TimeEntry {
 	private String out;
 	private String duration;
 	private String comments;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comments == null) ? 0 : comments.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((day == null) ? 0 : day.hashCode());
+		result = prime * result + ((duration == null) ? 0 : duration.hashCode());
+		result = prime * result + ((in == null) ? 0 : in.hashCode());
+		result = prime * result + ((out == null) ? 0 : out.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TimeEntry other = (TimeEntry) obj;
+		if (comments == null) {
+			if (other.comments != null)
+				return false;
+		} else if (!comments.equals(other.comments))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (day == null) {
+			if (other.day != null)
+				return false;
+		} else if (!day.equals(other.day))
+			return false;
+		if (duration == null) {
+			if (other.duration != null)
+				return false;
+		} else if (!duration.equals(other.duration))
+			return false;
+		if (in == null) {
+			if (other.in != null)
+				return false;
+		} else if (!in.equals(other.in))
+			return false;
+		if (out == null) {
+			if (other.out != null)
+				return false;
+		} else if (!out.equals(other.out))
+			return false;
+		return true;
+	}
 
 }
--- src/com/hcl/bonusreport/services/BonusReportService.java
+++ src/com/hcl/bonusreport/services/BonusReportService.java
@@ -2,7 +2,11 @@ package com.hcl.bonusreport.services;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -11,49 +15,128 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+import com.hcl.bonusreport.model.TimeEntry;
+
 public class BonusReportService {
+
+	private static final DataFormatter DATA_FORMATTER = new DataFormatter();
 
 	public static void readAttendanceFile(Path path) {
 
 	}
 
 	public static void readAttendanceFile(File file) {
+		Map<String, List<Row>> usersData = getUsersData(file);
+
+		usersData.forEach((k, v) -> {
+			if (null != v) {
+				System.out.println("User: " + k + "no filtered entries: " + v.size());
+				System.out.println("User: " + k + "filtered entries: " + getEntries(v).size());
+
+				getEntries(v).forEach(System.out::println);
+
+			}
+		});
+
+	}
+
+	private static Map<String, List<Row>> getUsersData(File file) {
+		Map<String, List<Row>> someData = new HashMap<>();
 		try {
-			Workbook workbook = WorkbookFactory.create(file);
-			// Retrieving the number of sheets in the Workbook
-			System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+			final Workbook workbook = WorkbookFactory.create(file);
+			final Sheet sheet = workbook.getSheetAt(0);
 
-			Iterator<Sheet> sheetIterator = workbook.sheetIterator();
-			System.out.println("Retrieving Sheets using Iterator");
-			while (sheetIterator.hasNext()) {
-				Sheet sheet = sheetIterator.next();
-				System.out.println("=> " + sheet.getSheetName());
-			}
-
-			// Getting the Sheet at index zero
-			Sheet sheet = workbook.getSheetAt(0);
-
-			// Create a DataFormatter to format and get each cell's value as
-			// String
-			DataFormatter dataFormatter = new DataFormatter();
+			List<Row> rows = new ArrayList<>();
+			String userName = null;
 			for (Row row : sheet) {
+				boolean isUser = false;
 				for (Cell cell : row) {
-					String cellValue = dataFormatter.formatCellValue(cell);
-					if (!cellValue.trim().isEmpty()) {
-						System.out.print(cellValue + "\t");
-					}
-				}
-				System.out.println();
-			}
+					String cellValue = DATA_FORMATTER.formatCellValue(cell);
 
+					if (cellValue.trim().toLowerCase().contains("summaries")) {
+						someData.replace(userName, rows);
+						rows = new ArrayList<>();
+						userName = null;
+						break;
+					}
+
+					if (isUser && !cellValue.trim().isEmpty()) {
+						userName = cellValue;
+						someData.put(userName, null);
+						break;
+					}
+
+					if (null != userName && someData.containsKey(userName) && null == someData.get(userName)
+							&& getRowCount(row) > 4 && getRowCount(row) < 7) {
+						rows.add(row);
+					}
+
+					isUser = isUser ? true : cellValue.toLowerCase().contains("user");
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+		return someData;
 	}
 
-	public static void readAttendanceFile(String path) {
+	private static void printRow(Row row) {
+		StringBuilder builder = new StringBuilder();
+		for (Cell c : row) {
+			builder.append(DATA_FORMATTER.formatCellValue(c)).append(" ");
+		}
 
+		System.out.println(builder);
+	}
+
+	private static int getRowCount(Row row) {
+		int i = 0;
+		for (Cell cell : row) {
+			String cellValue = DATA_FORMATTER.formatCellValue(cell);
+
+			if (!cellValue.trim().isEmpty()) {
+				i++;
+			}
+		}
+		return i;
+	}
+
+	private static List<TimeEntry> getEntries(List<Row> rows) {
+		List<TimeEntry> entries = new ArrayList<>();
+		for (Row row : rows) {
+
+			int cellCounter = 0;
+			TimeEntry entry = new TimeEntry();
+			for (Cell cell : row) {
+				String cellValue = DATA_FORMATTER.formatCellValue(cell);
+				if (!cellValue.trim().isEmpty()) {
+					switch (cellCounter++) {
+					case 0:
+						entry.setDate(cellValue);
+						break;
+					case 1:
+						entry.setDay(cellValue);
+						break;
+					case 2:
+						entry.setIn(cellValue);
+						break;
+					case 3:
+						entry.setOut(cellValue);
+						break;
+					case 4:
+						entry.setDuration(cellValue);
+						break;
+					case 5:
+						entry.setComments(cellValue);
+						break;
+					}
+				}
+			}
+			entries.add(entry);
+		}
+
+		return entries.stream().distinct().collect(Collectors.toList());
 	}
 
 }
